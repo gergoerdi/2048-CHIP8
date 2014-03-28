3a3975ff5c94eb8fd5ceb6cb529e56c9cab2f765
Dr. ERDI Gergo <gergo@erdi.hu>
2014-03-28 20:46:24 +0800

    Implement sliding

--- 2048.asm
+++ 2048.asm
@@ -7,11 +7,26 @@
 ;;;
 ;;;
 START:
-        ;; call NEWGAME
         call DRAW_GRID
+
+        ;; call SLIDE_UP
+        ;; call BLIT
+
+        ;; call DRAW
+LOOP:
+        call DRAW
+        load va, key
         call DRAW
 
-HALT:   jump HALT
+        skip.ne va, 6
+        call SLIDE_RIGHT
+        skip.ne va, 4
+        call SLIDE_LEFT
+        skip.ne va, 8
+        call SLIDE_DOWN
+        skip.ne va, 2
+        call SLIDE_UP
+        jump LOOP
 
 DRAW_GRID:
         load i, GRID
@@ -37,6 +52,177 @@ DRAW_GRID_LOOP:
         skip.ne v3, 24          ; for bottommost row, use special sprite
         load i, GRID_BOTTOM
         jump DRAW_GRID_LOOP
+
+BLIT:
+        load v1, 0
+BLIT_LOOP:
+        skip.ne v1, 16
+        ret
+        load i, NEW_BOARD
+        add i, v1
+        restore v0
+        load i, BOARD
+        add i, v1
+        save v0
+        load v0, 0
+        load i, NEW_BOARD
+        add i, v1
+        save v0
+        add v1, 1
+        jump BLIT_LOOP
+
+;;; ROTATE
+;;;
+;;; Maps
+;;;
+;;; 0123
+;;; 4567
+;;; 89ab
+;;; cdef
+;;;
+;;; to
+;;;
+;;; c840
+;;; d951
+;;; ea62
+;;; fd73
+;;;
+;;; Use: V0: TMP
+;;;      V1: K
+;;;      V2: K'
+;;;      V3: ROW
+;;;      V4: NEW_ROW
+ROTATE:
+        call ROTATE_INT
+        call BLIT
+        ret
+ROTATE_INT:
+        load v1, 0
+        load v2, 3
+        load v3, 3
+ROTATE_LOOP:
+        skip.ne v1, 16
+        ret
+
+        load i, BOARD           ; TMP := BOARD[K]
+        add i, v1
+        restore v0
+
+        load i, NEW_BOARD       ; NEW_BOARD[K'] := TMP
+        add i, v2
+        save v0
+
+        add v1, 1                ; K += 1
+        add v2, 4                ; K' += 4
+
+        load v4, $11             ; New row?
+        and v4, v1
+        skip.eq v4, 0
+        jump ROTATE_LOOP
+
+        sub v3, 1               ; ROW -= 1
+        load v2, v3             ; K' := ROW
+        jump ROTATE_LOOP
+
+;;; SLIDE_LEFT
+;;; Out: VF: WIN
+;;; Use: V0: TMP
+;;;      V1: K
+;;;      V2: K'
+;;;      V3: LAST
+;;;      V4: END_ROW
+;;;      V5: ROW
+;;;      V6: MATCH
+SLIDE_LEFT:
+        call SLIDE_LEFT_INT
+        call BLIT
+        ret
+
+SLIDE_LEFT_INT:
+        load v1, 0              ; K := 0
+        load v2, 0              ; K' := 0
+        load v3, 0              ; LAST := 0
+        load vf, 0              ; WIN := 0
+        load v5, 0              ; ROW := 0
+
+SLIDE_LEFT_ROW:
+        skip.ne v1, 16
+        ret
+
+        load i, BOARD           ; TMP := BOARD[K]
+        add i, v1
+        restore v0
+
+        skip.ne v0, 0
+        jump SLIDE_LEFT_NEXT
+
+        load v6, 0
+        skip.ne v0, v3
+        load v6, 1
+
+        skip.ne v6, 1          ; IF TMP == LAST then merge
+        call MERGE_LEFT
+        skip.eq v6, 1
+        load v3, v0
+
+        load i, NEW_BOARD       ; NEW_BOARD[K'] := TMP'
+        add i, v2
+        save v0
+
+        add v2, 1               ; K' += 1
+
+SLIDE_LEFT_NEXT:
+        add v1, 1               ; K += 1
+
+        load v4, $11            ; New row?
+        and v4, v1
+        skip.eq v4, 0
+        jump SLIDE_LEFT_ROW
+
+        add v5, 1               ; ROW += 1
+        load v3, 0              ; LAST := 0
+        load v2, 0              ; K' := ROW * 4
+        skip.ne v5, 1
+        load v2, 4
+        skip.ne v5, 2
+        load v2, 8
+        skip.ne v5, 3
+        load v2, 12
+        jump SLIDE_LEFT_ROW
+
+SLIDE_DOWN:
+        call ROTATE
+        call SLIDE_LEFT
+        call ROTATE
+        call ROTATE
+        call ROTATE
+        ret
+
+SLIDE_RIGHT:
+        call ROTATE
+        call ROTATE
+        call SLIDE_LEFT
+        call ROTATE
+        call ROTATE
+        ret
+
+SLIDE_UP:
+        call ROTATE
+        call ROTATE
+        call ROTATE
+        call SLIDE_LEFT
+        call ROTATE
+        ret
+
+;;; MERGE
+;;; InOut: V0: TMP
+;;;        V2: K'
+;;;        V3: LAST
+MERGE_LEFT:
+        add v0, 1
+        load v3, 0
+        sub v2, 1
+        ret
 
 ;;; DRAW_TILE
 ;;; In:  V1: K
@@ -76,35 +262,41 @@ DRAW_LOOP:
 
 ;;; NEWGAME: fill BOARD with zeroes
 NEWGAME:
-        load i, BOARD
+        load v1, 0
         load v0, 0
-        load v1, 1
-        load v2, 16
 NEWGAME_LOOP:
-        skip.ne v2, 0
+        skip.ne v1, 16
         ret
-        save v0
+        load i, BOARD
         add i, v1
+        save v0
+        add v1, 1
         jump NEWGAME_LOOP
 
 BOARD:
         ;; .ds 16, 0x01
+        .byte 0
         .byte 1
+        .byte 0
         .byte 1
+
+        .byte 3
         .byte 0
         .byte 0
+        .byte 5
+
         .byte 0
+        .byte 1
+        .byte 4
         .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
-        .byte 0
+
+        .byte 7
+        .byte 7
+        .byte 7
+        .byte 6
+
+NEW_BOARD:
+        .ds 16
 
 GRID:   .byte $11111111
         .byte $10000000
